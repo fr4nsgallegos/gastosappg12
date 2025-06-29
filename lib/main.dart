b21d6a1d24ecd4b5bc0d1af82734f93a5f425947
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-06-29 11:27:49 -0500

    first

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:gastosappg12/pages/examples/db_example.dart';
+
+void main() {
+  runApp(MaterialApp(home: DbExample()));
+}
---
+++ lib/pages/examples/db_example.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DbExample extends StatelessWidget {
+  const DbExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
