16b5d23aca7f61709b080af9b5b9b6619928b397
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-07-06 09:23:47 -0500

    first interdace

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gastosappg12/pages/examples/db_example.dart';
+import 'package:gastosappg12/pages/home_page.dart';
 
 void main() {
-  runApp(MaterialApp(home: DbExample()));
+  runApp(MaterialApp(home: HomePage()));
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  color: Colors.black,
+                  width: double.infinity,
+                  height: 120,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.add, color: Colors.white),
+                      SizedBox(width: 8),
+                      Text(
+                        "Agregar",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(25),
+                        bottomRight: Radius.circular(25),
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Ingresa tus gastos",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                        Text(
+                          "Gestiona tus gastos de mejor forma",
+                          style: TextStyle(color: Colors.black45, fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 100),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
